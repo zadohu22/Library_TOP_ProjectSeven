e371cf73136447c942764f68245cd17ff09c1091
zadohu <zadohu22@gmail.com>
2022-06-04 19:37:55 -0500

    adds basic functionality and layout.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let myLibrary = [];
-const title = document.getElementsByClassName('titleInput')[0].value;
-const author = document.getElementsByClassName('authorInput')[0].value;
-const pages = document.getElementsByClassName('pagesInput')[0].value;
-const haveRead = document.getElementsByClassName('readInput')[0].value;
+
+// const title = document.getElementsByClassName('titleInput')[0].value;
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const haveRead = document.getElementById('read')
 const submit = document.getElementsByClassName('submitInput')[0];
 const addBookButton = document.getElementById("addBook");
 const form = document.getElementById("form");
 const initDescription = document.getElementById('initDescription');
-
+const container = document.getElementById('container');
 
 function Book(title, author, pages, haveRead){
     this.title = title;
@@ -26,59 +27,117 @@ function bringUpForm(){
     addBookButton.addEventListener('click', function(){
         initDescription.className = "hideDescription";
         form.classList.toggle('hideForm');
+        console.log(myLibrary)
     });
     
 }
 
-
+// SUBMIT BUTTON ON FORM 
 function addBookToLibrary(){
     // add book.
     submit.addEventListener('click', function(){
+        myLibrary = [];
         // create new Book object from user input
-        let book1 = new Book(title, author, pages, haveRead);
-        // console.log(book1)
-        let object = book1.bookInfo();
-        myLibrary.push(object);
+        myLibrary.push(new Book(title.value, author.value, pages.value, haveRead.value).bookInfo())
         form.className = "hideForm";
         console.log(myLibrary);
-        console.log(typeof myLibrary[0])
+        
+        displayBooks();
     })
 }
 
 function displayBooks(){
     myLibrary.forEach(element => {
-        //is a string, convert to array at ,
-        //let newArray = element.split
-        // use nth child 1 for array[0]
-        // etc....
+        // let newDiv = document.createElement('div');
+        // newDiv.classList.add('card');
+        // newDiv.textContent = `Title: ${title.value} Author: ${author.value} Pages: ${pages.value}`
+        // container.appendChild(newDiv);
+        
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('card');
+        container.appendChild(newDiv);
+        
+        let nameContainer = document.createElement('div');
+        nameContainer.classList.add('bookNameContainer');
+        newDiv.appendChild(nameContainer);
+        
+        let nameParent = document.createElement('p');
+        nameParent.classList.add('bookNameParent');
+        nameParent.textContent = 'Title:';
+        nameContainer.appendChild(nameParent);
+        
+        let nameChild = document.createElement('p');
+        nameChild.classList.add('bookNameChild');
+        nameChild.textContent = title.value;
+        nameContainer.appendChild(nameChild);
+        
+        function makeHr(){
+            let hr = document.createElement('hr');
+            newDiv.appendChild(hr);
+        }
+        
+        makeHr();
+        // ***********************************
 
-        /* that won't work, i'd have to create every element for every iteration
-            for example:
-            createElement div with class card
-            create p class bookNameParent
-            p.textContent = Title:
-            append p to div.card
-            create p.bookNameChild
-            bookNameChild.textContent = newArray[0]
-            append p to div.card
-            create hr
-            append hr to div.card
+        let authorContainer = document.createElement('div');
+        authorContainer.classList.add('authorNameContainer');
+        newDiv.appendChild(authorContainer);
+        
+        let authorParent = document.createElement('p');
+        authorParent.classList.add('authorNameParent');
+        authorParent.textContent = 'Author:';
+        authorContainer.appendChild(authorParent);
+        
+        let authorChild = document.createElement('p');
+        authorChild.classList.add('authorNameChild');
+        authorChild.textContent = author.value;
+        authorContainer.appendChild(authorChild);
 
-            and do over and over every time 
+        makeHr();
+        // ***********************************
 
-        */
+        let pagesContainer = document.createElement('div');
+        pagesContainer.classList.add('pagesNameContainer');
+        newDiv.appendChild(pagesContainer);
+        
+        let pagesParent = document.createElement('p');
+        pagesParent.classList.add('pagesNameParent');
+        pagesParent.textContent = 'Pages:';
+        pagesContainer.appendChild(pagesParent);
+        
+        let pagesChild = document.createElement('p');
+        pagesChild.classList.add('pagesNameChild');
+        pagesChild.textContent = pages.value;
+        pagesContainer.appendChild(pagesChild);
 
-            //there has to be a better way. quitting for now, tackle tomorrow. 
+        makeHr();
+
+        //********************************* */
+
+        let readContainer = document.createElement('div')
+        readContainer.classList.add('readContainer');
+        newDiv.appendChild(readContainer);
+        
+        let readLabel = document.createElement('label');
+        readLabel.setAttribute('for', read);
+        readLabel.classList.add('readLabel');
+        readLabel.textContent = 'Read?';
+        readContainer.appendChild(readLabel);
+
+        let readCheckBox = document.createElement('input');
+        readCheckBox.setAttribute('type', 'checkbox');
+        readCheckBox.setAttribute('name', 'read');
+        if(haveRead.checked){
+            readCheckBox.setAttribute('checked', 'checked');
+        }
+        readCheckBox.classList.add('read');
+        readContainer.appendChild(readCheckBox);
+        
     })
 }
 
 bringUpForm();
 addBookToLibrary();
+displayBooks()
 
-// function createDivs(){
-//     const cardContainer = document.createElement('div');
-// }
 
-// function displayBooks(){
-//     myLibrary.forEach(element => )
-// }
